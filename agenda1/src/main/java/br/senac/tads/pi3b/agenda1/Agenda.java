96468f3fd45881d6d1f1a77d7fc38317de8f7b77
bruna.gagliardi <bruna.gagliardi@CAS511766W1029.senacsp.edu.br>
2018-02-27 20:39:08 -0300

    Versão Inicial

---
+++ agenda1/src/main/java/br/senac/tads/pi3b/agenda1/Agenda.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3b.agenda1;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author bruna.gagliardi
+ */
+public class Agenda {
+
+    private Connection obterConexão() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/agendadb",
+                "root",
+                "");
+        return conn;
+    }
+
+    public void listar() throws ClassNotFoundException, SQLException {
+
+        try (Connection conn = obterConexão();
+                PreparedStatement stmt = conn.prepareStatement("SELECT id, nome, dtnascimento FROM PESSOA");
+                ResultSet resultados = stmt.executeQuery();) {
+
+            while (resultados.next()) {
+                long id = resultados.getLong("id");
+                String nome = resultados.getString("nome");
+                Date dtnascimento = resultados.getDate("dtnascimento");
+
+                System.out.println(id + ", " + nome + ", " + dtnascimento);
+            }
+
+        }
+
+    }
+
+    public void incluir() throws ClassNotFoundException, SQLException {
+
+        try (Connection conn = obterConexão();
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO PESSOA (nome, dtnascimento) VALUES (?,?)")) {
+            stmt.setString(1, "MARIA DE SOUZA");
+            GregorianCalendar cal = new GregorianCalendar(1992, 10, 5); //5 de novembro de 1992
+            stmt.setDate(2, new java.sql.Date(cal.getTimeInMillis()));
+
+            int status = stmt.executeUpdate();
+            System.out.println("Status: " + status);
+        }
+    }
+
+    public static void main(String[] args) {
+        Agenda agenda = new Agenda();
+        try {
+            agenda.incluir();
+            agenda.listar();
+        } catch (ClassNotFoundException ex) {
+            System.err.print(ex.getMessage());
+        } catch (SQLException ex) {
+            System.err.print(ex.getMessage());
+        }
+        
+        
+    }
+}
